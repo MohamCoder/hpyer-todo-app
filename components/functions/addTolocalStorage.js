00a8d4420c519f0bc63488926e8c02a30119fd1d
MohamCoder <125498068+MohamCoder@users.noreply.github.com>
2024-05-17 23:40:14 +0300

    Update addTolocalStorage.js

--- components/functions/addTolocalStorage.js
+++ components/functions/addTolocalStorage.js
@@ -1,7 +1,7 @@
 export function addToLocalStorage(todoList) {
   const todoListCopy = JSON.parse(JSON.stringify(todoList))
   todoListCopy.forEach(todo => {
-    todo.deadline = new Date(todo.deadline).toLocaleDateString()
+    todo.deadline = [todo.deadline.getday(),todo.deadline.getHours(),todo.deadline.getMinutes()]
   })
   console.log(todoListCopy)
   localStorage.setItem("todoList", JSON.stringify(todoListCopy))
