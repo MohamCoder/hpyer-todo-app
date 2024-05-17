52cf1b27289fff878b577d96d5513df2cd0edda3
MohamCoder <125498068+MohamCoder@users.noreply.github.com>
2024-05-17 23:57:50 +0300

    Update todos.js

--- src/hero/todos.js
+++ src/hero/todos.js
@@ -6,11 +6,7 @@ export function todos(parentElement,todoList,todosConfig) {
   todos.className = "mt-16 lg:mx-64 md:mx-32 mx-8 flex flex-col space-y-8"
   todos.id = "todos"
   for (const todo of todoList) {
-    const date = new Date()
-    date.setDay(todo.deadline[0])
-    date.setHours(todo.deadline[1])
-    dare.getMinutes(todo.deadline[2])
-    todo.deadline = date
+    todo.deadline = new Date(todo.deadline)
     const interval = todoElement(todos, todo, todoList,todosConfig)
     todo.interval = interval
   }
